4b341eab23228c4e621e5dcbc5bd4bd78adb41bf
nathanloo87 <loon1@uci.edu>
2025-03-16 09:44:45 +0000

    remove error check test

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -36,10 +36,10 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     return path;
 };
 void print_path(const vector<int>& v, int total) {
-    if (v.empty() || total == INF) {
-        cout << "No path found" << endl;
-        return;
-    }
+    // if (v.empty() || total == INF) {
+    //     cout << "No path found" << endl;
+    //     return;
+    // }
     for (size_t i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
     }
