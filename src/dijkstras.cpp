a2ec212df483834092f480b48798eecf9cdf6368
nathanloo87 <loon1@uci.edu>
2025-03-16 00:16:46 +0000

    referenced changes, heap overflow

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,16 +1,16 @@
 #include "dijkstras.h"
+#include <algorithm>
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int vertices = G.numVertices;
     vector<int> distances(vertices, INF);
-    previous.assign(vertices, -1);
     vector<bool> visited(vertices, false);
     distances[source] = 0;
     previous[source] = -1; // Undefined = -1
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap; //pair<vertex, weight>
-    minHeap.push({source, 0});
+    minHeap.push(make_pair(source, 0));
     while (!minHeap.empty()) {
-        auto [dist, u] = minHeap.top();
+        int u = minHeap.top().second; // stored in a set [index, value]
         minHeap.pop();
         if (visited[u]) continue;
         visited[u] = true;
@@ -20,7 +20,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             if (!visited[v] && distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v, distances[v]});
+                minHeap.push(make_pair(v, distances[v]));
             }
         }
     }
