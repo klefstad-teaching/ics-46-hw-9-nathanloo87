8b05a385913e56e5885e0da833dda26e61bf3d63
nathanloo87 <loon1@uci.edu>
2025-03-16 07:10:23 +0000

    from instructions

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,4 +1,5 @@
 #include "ladder.h"
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
 
 void error(string word1, string word2, string msg) {
@@ -87,12 +88,12 @@ void print_word_ladder(const vector<string>& ladder) {
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
-    assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-    assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-    assert(generate_word_ladder("code", "data", word_list).size() == 6);
-    assert(generate_word_ladder("work", "play", word_list).size() == 6);
-    assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
-    assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 };
 
 /*
